bc95dd95b88a40a203430b761f0786e2121e0390
nikkon2001@mail.ru <nikkon2301>
2022-02-24 17:47:21 +0500

    Добавил data класс и в цикле выводил его объекты

--- app/src/main/java/com/example/testkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/testkotlinproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testkotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import com.example.testkotlinproject.R
@@ -10,7 +11,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var button = findViewById<Button>(R.id.main_activity__button)
-        button.setOnClickListener { Toast.makeText(this,"Hello!",Toast.LENGTH_LONG).show() }
+        val button = findViewById<Button>(R.id.main_activity__button)
+        button.setOnClickListener { Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show() }
+        val person = Person("Ivan","Ivanov")
+        Log.e("111",person.toString())
+        val newCopy = person.copy()
+        Log.e("111",newCopy.toString())
+        val personList = ArrayList<Person>()
+        personList.add(Person("Petr","Petrov"))
+        personList.add(person)
+        personList.add(Person("Igor","Igorev"))
+        for(persons in personList){
+            Log.e("@@@",persons.toString())
+        }
+
+        for(i in 30 downTo 1 step 3) {
+            Log.e("@@@","Hello Kotlin!")
+        }
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/testkotlinproject/Person.kt
@@ -0,0 +1,3 @@
+package com.example.testkotlinproject
+
+data class Person(val firstName: String, val secondName: String)
